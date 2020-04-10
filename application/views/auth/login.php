0d1f4c147ad38caf04c9ce240628dcb425190b7e
redxx <faishalraihan123@gmail.com>
2020-04-10 16:28:57 +0700

    update style.css

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -2,47 +2,46 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Login</title>
+    <link href="<?= base_url('assets/css/style.css') ?> " rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link type="text/css" rel="stylesheet" href="assets/css/style.css">
+
 </head>
 
 <body class="body_login">
 
     <div class="container">
 
-
+        <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-lg-7">
+            <div class=" col-lg-7">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
-
+                        <!-- Nested Row within Card Body -->
                         <div class="row">
-
                             <div class="col-lg">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Login Page</kh1>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Username">
+                                            <input type="text" class="form-control form-control-user" placeholder="Enter Username..." name="username">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                            <input type="password" class="form-control form-control-user" placeholder="Password" name="password">
                                         </div>
-                                        <button type="submit" class="button_login">
+                                        <button class="btn_login">
                                             Login
                                         </button>
 
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="pass_link" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="pass_link" href="">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
                                         <a class="acc_link" href="">Create an Account!</a>
@@ -59,12 +58,10 @@
 
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-
-
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
