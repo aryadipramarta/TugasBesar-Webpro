15579b2bac0c22677137e9f3a6c87a0f9317ee41
redxx <47036422+faishalraihan@users.noreply.github.com>
2020-04-13 20:57:01 +0700

    Add files via upload

--- db_tubeswebpro.sql
+++ db_tubeswebpro.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2020 at 05:56 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.2
+-- Generation Time: Apr 13, 2020 at 03:56 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,63 +25,77 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pasien`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `pasien` (
-  `id_pasien` int(10) NOT NULL,
-  `full_name` varchar(100) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(15) NOT NULL
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `name`, `username`, `email`, `password`, `role_id`) VALUES
+(1, 'Faishal Raihan', 'redxx', 'faishalraihan73@gmail.com', '$2y$10$X6c5LBy0TxMn/TVIq3UvAO65X/QeEXxC73OLwfSqRq.rmcVPm4u2q', 2),
+(2, 'Saipul Jamil', 'haphaphap', 'saipulhap@gmail.com', '$2y$10$evOcqxEDUYGVzInIl8wrLeVG2ZmU6O628/5M1qoswds76JjcadGGm', 2),
+(3, 'Panjul Jukipli', 'Panjulll', 'panjul@gmail.com', '$2y$10$JifpR/ioi.cdOSyAWlduRukcc/Y616B/gz7m60oRBTNxgufr2qA.m', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produk`
+-- Table structure for table `user_role`
 --
 
-CREATE TABLE `produk` (
-  `id` int(5) NOT NULL,
-  `tipe_produk` varchar(20) NOT NULL,
-  `nama_produk` varchar(100) NOT NULL,
-  `harga` int(100) NOT NULL,
-  `deskripsi` varchar(100) NOT NULL
+CREATE TABLE `user_role` (
+  `role_id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`role_id`, `role`) VALUES
+(1, 'Dokter'),
+(2, 'Customer');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `pasien`
+-- Indexes for table `user`
 --
-ALTER TABLE `pasien`
-  ADD PRIMARY KEY (`id_pasien`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
 
 --
--- Indexes for table `produk`
+-- Indexes for table `user_role`
 --
-ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`role_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `pasien`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `pasien`
-  MODIFY `id_pasien` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `produk`
+-- AUTO_INCREMENT for table `user_role`
 --
-ALTER TABLE `produk`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user_role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
