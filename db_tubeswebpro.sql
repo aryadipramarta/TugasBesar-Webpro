003524c4cd79553db31a03a1492a4b2f70788beb
beginner75 <44922464+adhityahardy@users.noreply.github.com>
2020-04-17 18:14:38 +0700

    update db

--- db_tubeswebpro.sql
+++ db_tubeswebpro.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 13, 2020 at 03:56 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: Apr 17, 2020 at 01:13 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,47 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_tubeswebpro`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id_pasien` int(10) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(5) NOT NULL,
+  `tipe_produk` varchar(20) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `harga` int(100) NOT NULL,
+  `deskripsi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id`, `tipe_produk`, `nama_produk`, `harga`, `deskripsi`) VALUES
+(1, 'Facewash', 'SkinSaver Gentle Skin Cleanser', 75000, 'produk ini adalah produk andalan kami yang terbukti sangat cocok untuk membersihkan jerawat'),
+(2, 'Facewash', 'SkinSaver Cleanser', 80000, 'produk yang sangat baik mengandung 2% Benzoyl Peroxide dan eskrak buah lainnya'),
+(3, 'Moisturizer', 'SkinSaver Barrier Moisturizer', 100000, 'melembabkan kulit sangat cocok untuk kulit berjerawat mengandung Ceramide'),
+(4, 'Moisturizer', 'SkinSaver LightDay Moisturizer', 120000, 'melembabkan kulit sangat baik, cocok segala jenis kulit mengandung SPF 50+/PA++++'),
+(5, 'Acne Spot', 'SkinSaver Acne Spot treatment', 60000, 'produk terbaik untuk jerawat dengan 2.5% Benzoyl Peroxide'),
+(6, 'Serum', 'SkinSaver Niacinamide and Zink Serum', 140000, 'serum terbaik untuk jerawat anda, mengandung Niacinamide 10% and Zink 1%');
 
 -- --------------------------------------------------------
 
@@ -70,6 +111,18 @@ INSERT INTO `user_role` (`role_id`, `role`) VALUES
 --
 
 --
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id_pasien`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -84,6 +137,18 @@ ALTER TABLE `user_role`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id_pasien` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
