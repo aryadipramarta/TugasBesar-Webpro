befd3e7bcd145eb799885e27de160eaf9de27b17
aryadipramarta <46791784+aryadipramarta@users.noreply.github.com>
2020-04-18 09:56:33 +0700

    updateproduct
    updateproduct homepage utama

--- application/views/landingpage/homepage_product.php
+++ application/views/landingpage/homepage_product.php
@@ -72,93 +72,51 @@
       <h2 class="isibanneruser"><b>OUR PRODUCT</b></h2>
     </div>
     <div class="d-flex justify-content-center">
-        <div class="row text-center">
-          <div class="col">
-            <img src="<?= base_url('assets/image/facewash1.png') ?>" class="product-card">
-          </div>
-          <div class="col">
-            <img src="<?= base_url('assets/image/facewash2.png') ?>" class="product-card">
-          </div>                
-          <div class="col">
-            <img src="<?= base_url('assets/image/moist1.png') ?>" class="product-card">
-          </div>
-          <div class="col">
-            <img src="<?= base_url('assets/image/moist2.png') ?>" class="product-card">
-          </div>
-            <div class="col">
-               <img src="<?= base_url('assets/image/acnespot.png') ?>" class="product-card">
-            </div>
-              <img src="<?= base_url('assets/image/serum.png') ?>" class="product-card">
-          </div>
-        </div>
-</div>
+        <br>
+        <img src="<?= base_url('assets/image/contact.png') ?>" class="about-banner">
+      </div>
+    <img src="<?= base_url('assets/image/wave1.png') ?>" class="bottom-img">
   </section>
 
   <!--Service-->
 <section class="services">
-  <div class="d-flex justify-content-center">
-    <div class="card">
-      <div class="card-body">
-        <div class="row">
-            <div class="col-md-auto">
-                <i class="fab fa-facebook contact"></i>
-            </div>
-            <div class="col">
-                <br>
-                <h1><b>CONTACT ON FACEBOOK</b></h1>
-                <p>SKIN SAVER CLINIC </p>
-            </div>
-        </div>
-        </div>
-    </div>
-  </div>
-  <div class="d-flex justify-content-center">
-    <div class="card">
-      <div class="card-body">
-        <div class="row">
-            <div class="col">
-                <br>
-                <h1><b>CONTACT ON INSTAGRAM</b></h1>
-                <p>SKIN SAVER CLINIC </p>
-            </div>
-            <div class="col-md-auto">
-                <i class="fab fa-instagram contact"></i>
-            </div>
-        </div>
-        </div>
-    </div>
-  </div>
-  <div class="d-flex justify-content-center">
-    <div class="card">
-      <div class="card-body">
-        <div class="row">
-            <div class="col-md-auto">
-                <i class="fab fa-whatsapp contact"></i>
-            </div>
-            <div class="col">
-                <br>
-                <h1><b>CONTACT ON WHATSAPP</b></h1>
-                <p>SKIN SAVER CLINIC (+628123456789) </p>
-            </div>
-        </div>
-        </div>
-    </div>
-  </div>
-  <div class="d-flex justify-content-center">
-    <div class="card">
-      <div class="card-body">
-        <div class="row">
-            <div class="col">
-                <br>
-                <h1><b>OUR CLINIC LOCATION</b></h1>
-                <p>Jl. Telekomunikasi Jl. Terusan Buah Batu, Sukapura, Kec. Dayeuhkolot, Bandung, Jawa Barat 40257</p>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.296326309934!2d107.62914351472088!3d-6.974323194961576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6285c5b7da517%3A0x864485f26a388f95!2sTelkom%20University!5e0!3m2!1sid!2sid!4v1587082414410!5m2!1sid!2sid" width="500" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-            </div>
-            <div class="col-md-auto">
-                <i class="fas fa-map-marked-alt contact"></i>
-            </div>
-        </div>
-        </div>
-    </div>
-  </div>
+    <div class="d-flex justify-content-center">
+        <div class="row text-center">
+          <div class="col product-card">
+            <h2 class="judulproduct"><b>NAMA PRODUK</b></h2>
+            <img src="<?= base_url('assets/image/facewash1.png') ?>" class="gambarproduct">
+            <p>Isi Didalam Deskripsi</p>
+          </div>
+          <div class="col product-card">
+            <h2 class="judulproduct"><b>NAMA PRODUK</b></h2>
+            <img src="<?= base_url('assets/image/facewash2.png') ?>" class="gambarproduct">
+            <p>Isi Didalam Deskripsi</p>
+          </div>                
+          <div class="col product-card">
+            <h2 class="judulproduct"><b>NAMA PRODUK</b></h2>
+            <img src="<?= base_url('assets/image/moist1.png') ?>" class="gambarproduct">
+            <p>Isi Didalam Deskripsi</p>
+          </div>
+      </div>
+</div>
+<div class="d-flex justify-content-center">
+        <div class="row text-center">
+          <div class="col product-card">
+          <h2 class="judulproduct"><b>NAMA PRODUK</b></h2>
+            <img src="<?= base_url('assets/image/moist2.png') ?>" class="gambarproduct">
+            <p>Isi Didalam Deskripsi</p>
+          </div>
+          <div class="col product-card">
+          <h2 class="judulproduct"><b>NAMA PRODUK</b></h2>
+            <img src="<?= base_url('assets/image/acnespot.png') ?>" class="gambarproduct">
+            <p>Isi Didalam Deskripsi</p>
+          </div>                
+          <div class="col product-card">
+          <h2 class="judulproduct"><b>NAMA PRODUK</b></h2>
+            <img src="<?= base_url('assets/image/serum.png') ?>" class="gambarproduct">
+            <p>Isi Didalam Deskripsi</p>
+          </div>
+      </div>
+</div>
+</div>
 </section>
