685d8c5af587f5c06a5d64ca8dde72130b55151d
aryadipramarta <46791784+aryadipramarta@users.noreply.github.com>
2020-04-23 12:42:30 +0700

    dokter view update done
    dokter view update done

--- application/controllers/dokter.php
+++ application/controllers/dokter.php
@@ -5,7 +5,30 @@ class dokter extends CI_Controller
 
     public function index()
     {
+        $session = $this->session->userdata('username');
+        //if (!isset($session)) redirect('auth');
+        $this->load->model('authModel');
+        $user = $this->authModel->get_profile($session);
+        $this->load->view('user/dokter/home_dokter', ['data' => $user]);
+        $this->load->view('template/menu_footer');
+    }
 
-        $this->load->view('user/home_dokter');
+    public function datapasien()
+    {
+        $session = $this->session->userdata('username');
+        //if (!isset($session)) redirect('auth');
+        $this->load->model('authModel');
+        $user = $this->authModel->get_profile($session);
+        $this->load->view('user/dokter/lihatpasien', ['data' => $user]);
+        $this->load->view('template/menu_footer');
+    }
+    public function editprofiledokter()
+    {
+        $session = $this->session->userdata('username');
+        //if (!isset($session)) redirect('auth');
+        $this->load->model('authModel');
+        $user = $this->authModel->get_profile($session);
+        $this->load->view('user/dokter/editprofiledokter', ['data' => $user]);
+        $this->load->view('template/menu_footer');
     }
 }
